6531c35e01737bd62e885c73bcebadf0377e7594
James <jwin4740@gmail.com>
2017-04-11 18:46:14 -0400

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection;
 var pool;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection({
-//     host: 'wvulqmhjj9tbtc1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//     user: 'dlm4rgt37q308ite',
-//     password: 'dayuxqnae55nyfwy',
-//     database: 'wgbgjjdudztlge8l'
-//   });
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'platinum',
-//     database: 'ww2_db'
-//   });
-// }
 
 if (process.env.JAWSDB_URL) {
   pool = mysql.createPool({
@@ -37,21 +20,12 @@ if (process.env.JAWSDB_URL) {
 }
 
 pool.getConnection(function (err, connection) {
-  // connected! (unless `err` is set) 
+    console.log("connected");
     if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
 });
-
-// Make connection.
-
-// connection.connect(function (err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-// });
 
 
 // Export pool for our ORM to use.
